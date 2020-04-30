6a70af0ec6836bfc1a56b059437a4e464a2c5968
amylinck <amy.peerlinck@student.montana.edu>
2020-04-29 21:08:44 -0600

    Change Transaction constructor to take in Json param

--- code/Transaction.py
+++ code/Transaction.py
@@ -1,25 +1,23 @@
 import hashlib, json, datetime
 
-class NewTransaction:
-    def newTx(_to: str, _from: str, amount: float) -> str:
-        unique_id = self.generateIDh()  # hash of to, from, amount, timestamp
-
-    def generateIDh():
-        hashMe = self.toJSON()
-        return hashlib.sha256(hashMe.encode()).hexdigest()
 
 class Transaction:
+    def __init__(self, json_string = '', _to: str = '', _from: str = '', amount: float = 0.0):
+        if json_string != '':
+            json_obj = json.loads(json_string)
+            self.to_node = json_obj['to_node']
+            self.from_node = json_obj['from_node']
+            self.amount = json_obj['amount']
+            self.timestamp = json_obj['timestamp']
+            self.unique_id = json_obj['unique_id']
+        else:
+            self.to_node = _to #node id
+            self.from_node = _from #own id
+            self.amount = amount
+            self.timestamp = str(datetime.datetime.now())
+            self.unique_id = self.generateIDh()  # hash of to, from, amount, timestamp
 
-    def __init__(self, json_string):
-        json_obj = json.loads(json_string)
-        self.to_node = json_obj['to_node'] #node id
-        self.from_node =json_obj['from_node'] #own id
-        self.amount = json_obj['amount']
-        self.timestamp = json_obj['timestamp']
-        self.uniqueID = json_obj
-
-
-    def toJSON(self):
+    def txHeaderToJSON(self):
         self_dict = {
             'to_node': self.to_node,
             'from_node': self.from_node,
@@ -28,6 +26,10 @@ class Transaction:
         }
         return json.dumps(self_dict)
 
+    def generateIDh(self):
+        hashMe = self.txHeaderToJSON()
+        return hashlib.sha256(hashMe.encode()).hexdigest()
+
     def __str__(self):
         return json.dumps(self.__dict__)
 
@@ -35,11 +37,12 @@ class Transaction:
         return _in.unique_id == self.unique_id
 
 
+
 if __name__ == '__main__':
-    t = Transaction('node1', 'node2', 12.5)
+    t = Transaction(_to='node1', _from='node2', amount=12.5)
     t.unique_id = t.generateIDh()
     print(str(t))
     jsonT = str(t)
-    backToT = jsonToTx(jsonT)
+    backToT = Transaction(json_string=jsonT)
     print(type(backToT))
     print(str(backToT))
